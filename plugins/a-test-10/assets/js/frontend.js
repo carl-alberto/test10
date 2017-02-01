29ca77cde9863927fbe59b5ede526bfbec3a793c
carl alberto <cralberto11@gmail.com>
2017-02-01 13:05:24 +0800

    adding custom test

--- plugins/a-test-10/assets/js/frontend.js
+++ plugins/a-test-10/assets/js/frontend.js
@@ -1,3 +1,10 @@
 jQuery( document ).ready( function ( e ) {
-	
+	$.ajax({
+		console.log('test');
+
+		url: "http://localhost/test1/",
+		success: function( data ) {
+			alert( 'Your home page has ' + $(data).find('div').length + ' div elements.');
+		}
+	})
 });
